b6d9bbb0f9f8c2d4f651e9f54165d492b3b6ffd5
Brandon Ashley <tb@tba.sh>
2018-04-23 15:12:23 -0400

    chore(config): rm ExtractTextPlugin from dev

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,7 +1,6 @@
 const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
